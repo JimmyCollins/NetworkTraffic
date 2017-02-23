ea7c83b0251a016b4325a02002912f60e2ec89ce
Jimmy Collins <jimmy.b.collins@gmail.com>
2017-02-23 21:25:19 +0000

    Add ability for user to change chart type at runtime (using the Strategy Pattern).

--- NetworkTraffic/src/org/jimmycollins/networktraffic/view/MainUIController.java
+++ NetworkTraffic/src/org/jimmycollins/networktraffic/view/MainUIController.java
@@ -9,6 +9,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.stage.FileChooser;
@@ -25,7 +26,12 @@ public class MainUIController implements Initializable
     @FXML
     private TabPane tabPane;
     
+    @FXML
+    private Button showBarChartsButton;
+    
     private final ChartContext chartContext = new ChartContext();
+    
+    private TrafficFileStats stats;
     
     @FXML
     private void handleSelectTrafficFile(ActionEvent event)
@@ -37,38 +43,15 @@ public class MainUIController implements Initializable
         
         try
         {
-            TrafficFileStats stats = TrafficFileParser.ParseFile(file);
+            // Parse the traffic file
+            stats = TrafficFileParser.ParseFile(file);         
             
-            Map<String,Integer> topSourceIPAddresses = Utility.GetTopElements(stats.GetSourceHosts(), 5);
-            Map<String,Integer> topDestinationIPAddresses = Utility.GetTopElements(stats.GetDestinationHosts(), 5);
-            Map<String,Integer> topSourcePorts = Utility.GetTopElements(stats.GetSourcePorts(), 5);
-            Map<String,Integer> topDestinationPorts = Utility.GetTopElements(stats.GetDestinationPorts(), 5);
-            
+            // Show Pie Charts by default
             chartContext.setChartStrategy(new PieChartStrategy());
             
-            // Top Source IP Addresses
-            Tab sourceIPAddresses = chartContext.createChartTab(topSourceIPAddresses, "Top Source IP Addresses");
-            sourceIPAddresses.setText("Source IP");
-            tabPane.getTabs().add(sourceIPAddresses);
-            
-            // Top Destination IP Addresses
-            Tab destinationIPAddresses = chartContext.createChartTab(topDestinationIPAddresses, "Top Destination IP Addresses");
-            destinationIPAddresses.setText("Destination IP");
-            tabPane.getTabs().add(destinationIPAddresses);
-            
-            chartContext.setChartStrategy(new BarChartStrategy());
-            
-            // Top Source Ports
-            Tab sourcePorts = chartContext.createChartTab(topSourcePorts, "Top Source Ports");
-            sourcePorts.setText("Source Ports");
-            tabPane.getTabs().add(sourcePorts);
-            
-            // Top Destination Ports
-            Tab destinationPorts = chartContext.createChartTab(topDestinationPorts, "Top Destination Ports");
-            destinationPorts.setText("Destination Ports");
-            tabPane.getTabs().add(destinationPorts);
-            
-            // TODO: More Charts
+            // Draw the charts
+            drawCharts();
+            showBarChartsButton.setDisable(false);
         }
         catch(Exception ex)
         {
@@ -76,6 +59,45 @@ public class MainUIController implements Initializable
             Utility.Alert(AlertType.ERROR, "Error", ex.getMessage() + "\n" + ex.getStackTrace());
         }
 
+    }
+    
+    @FXML
+    private void handleSelectChangeGraphs(ActionEvent event)
+    {
+        // Allows user to show Bar Charts instead of Pie Charts
+        tabPane.getTabs().clear();     
+        chartContext.setChartStrategy(new BarChartStrategy());
+        drawCharts();
+    }
+    
+    private void drawCharts()
+    {
+        Map<String,Integer> topSourceIPAddresses = Utility.GetTopElements(stats.GetSourceHosts(), 5);
+        Map<String,Integer> topDestinationIPAddresses = Utility.GetTopElements(stats.GetDestinationHosts(), 5);
+        Map<String,Integer> topSourcePorts = Utility.GetTopElements(stats.GetSourcePorts(), 5);
+        Map<String,Integer> topDestinationPorts = Utility.GetTopElements(stats.GetDestinationPorts(), 5);
+
+        // Top Source IP Addresses
+        Tab sourceIPAddresses = chartContext.createChartTab(topSourceIPAddresses, "Top Source IP Addresses");
+        sourceIPAddresses.setText("Source IP");
+        tabPane.getTabs().add(sourceIPAddresses);
+
+        // Top Destination IP Addresses
+        Tab destinationIPAddresses = chartContext.createChartTab(topDestinationIPAddresses, "Top Destination IP Addresses");
+        destinationIPAddresses.setText("Destination IP");
+        tabPane.getTabs().add(destinationIPAddresses);
+
+        // Top Source Ports
+        Tab sourcePorts = chartContext.createChartTab(topSourcePorts, "Top Source Ports");
+        sourcePorts.setText("Source Ports");
+        tabPane.getTabs().add(sourcePorts);
+
+        // Top Destination Ports
+        Tab destinationPorts = chartContext.createChartTab(topDestinationPorts, "Top Destination Ports");
+        destinationPorts.setText("Destination Ports");
+        tabPane.getTabs().add(destinationPorts);
+        
+        // TODO: More Charts
     }
     
     @Override
